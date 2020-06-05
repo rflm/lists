568ebed59df51e84dca6a19f00ca6a44ff19fb51
Rafał Mularski <rmularski@prograils.com>
2020-06-05 19:53:39 +0200

    Fix eslint errors in app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,25 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.scss"
-import "bootstrap";
+import css from '../css/app.scss';
+import 'bootstrap';
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
-// in "webpack.config.js".
+// in 'webpack.config.js'.
 //
 // Import dependencies
 //
-import "phoenix_html"
+import 'phoenix_html';
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+// import socket from './socket'
 
-import form from "./common/form";
-import commonRemove from "./common/remove";
-import itemsCheck from "./items/check";
+import form from './common/form';
+import commonRemove from './common/remove';
+import itemsCheck from './items/check';
 
 form();
 commonRemove();
