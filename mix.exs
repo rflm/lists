3729bdcbef15cfabe47872caaea86356ee94e211
Rafał Mularski <rmularski@prograils.com>
2020-06-07 21:05:26 +0200

    Add argon2

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Lists.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:argon2_elixir, "~> 2.0"}
     ]
   end
 
